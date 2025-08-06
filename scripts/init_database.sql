2991dba617286bf2d6f6b145f6d3988e57613e05
SafaeBenabbou <safaesafae0223@gmail.com>
2025-08-06 09:05:09 +0000

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+use master;
+--Create the 'DataWarehouse' database
+create database DataWarehouse;
+use DataWarehouse;
+--Create Schemas
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
+GO
